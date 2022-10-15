78d1ae9b7959daca1363aca1c6de8effb169b76f
jabaww <jabaruddinsyukri@gmail.com>
2022-10-15 08:58:12 +0700

    origin master

---
+++ ClassCoomponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>komponen ini dibuat dengan class Component</h1>
+        <h2>Materi komponen</h2>
+      </div>
+    );
+  }
+}
+export default ClassComponent;
---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ClassComponent from './ClassCoomponent';
+
+class komponen extends React.Component {
+  render() {
+    return (
+      <div>
+        <ClassComponent />
+      </div>
+    );
+  }
+}
